5c8476b1ece1f37cd385ee6fa3e305ebb530f2b0
Andrey Danilov <danand@inbox.ru>
2018-08-15 23:24:34 +0300

    #1 WIP: Move tests to own class files grouped by methods

---
+++ Src/SimpleContainer.Tests/DispatcherTests.cs
@@ -0,0 +1,65 @@
+﻿using SimpleContainer.Tests.DummyTypes;
+
+using NUnit.Framework;
+
+namespace SimpleContainer.Tests
+{
+    [TestFixture]
+    public class DispatcherTests
+    {
+        [Test]
+        public void Dispatcher_Send_Singleton()
+        {
+            var container = Container.Create();
+
+            container.Register<ICustomEventHandler, CustomEventHandler>(Scope.Singleton);
+            container.Register<DummyInvoker>(Scope.Singleton);
+
+            container.RegisterEvent<ICustomEventHandler, CustomEventArgs>((handler, args) => handler.OnCustomEvent(args));
+
+            var invoker = container.Resolve<DummyInvoker>();
+            var eventHandler = container.Resolve<ICustomEventHandler>();
+
+            var expectedValue = new CustomEventArgs
+            {
+                flag = true,
+                id = 9,
+                name = "shine"
+            };
+
+            invoker.RaiseEvent(expectedValue);
+
+            var actualValue = eventHandler.ReceivedEventArgs;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [Test]
+        public void Dispatcher_Send_Factory()
+        {
+            var container = Container.Create();
+
+            container.Register<DummyInvoker>(Scope.Singleton);
+            container.RegisterFactory<CustomEventHandlerFactory>();
+            container.RegisterEvent<ICustomEventHandler, CustomEventArgs>((handler, args) => handler.OnCustomEvent(args));
+
+            var invoker = container.Resolve<DummyInvoker>();
+            var factory = container.Resolve<CustomEventHandlerFactory>();
+
+            var eventHandler = factory.Create();
+
+            var expectedValue = new CustomEventArgs
+            {
+                flag = true,
+                id = 9,
+                name = "shine"
+            };
+
+            invoker.RaiseEvent(expectedValue);
+
+            var actualValue = eventHandler.ReceivedEventArgs;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
---
+++ Src/SimpleContainer.Tests/ResolveTests.cs
@@ -0,0 +1,121 @@
+﻿using SimpleContainer.Tests.DummyTypes;
+
+using NUnit.Framework;
+
+namespace SimpleContainer.Tests
+{
+    [TestFixture]
+    public class ResolveTests
+    {
+        [Test]
+        public void Resolve_Transient()
+        {
+            var container = Container.Create();
+
+            container.Register<ICar, CarTruck>(Scope.Transient);
+
+            var carFirst = container.Resolve<ICar>();
+            var carSecond = container.Resolve<ICar>();
+
+            Assert.AreNotSame(carFirst, carSecond);
+        }
+
+        [Test]
+        public void Resolve_Transient_All_Generic()
+        {
+            const int EXPECTED_COUNT = 2;
+
+            var container = Container.Create();
+
+            container.Register<IColor>(typeof(ColorRed), typeof(ColorBlue));
+
+            var colors = container.ResolveAll<IColor>();
+            var actualCount = colors.Length;
+
+            Assert.AreEqual(EXPECTED_COUNT, actualCount);
+        }
+
+        [Test]
+        public void Resolve_Transient_All_NonGeneric_Explicit()
+        {
+            const int EXPECTED_COUNT = 2;
+
+            var container = Container.Create();
+
+            container.Register<IColor>(typeof(ColorRed), typeof(ColorBlue));
+
+            var colors = container.ResolveAll(typeof(IColor));
+            var actualCount = colors.Length;
+
+            Assert.AreEqual(EXPECTED_COUNT, actualCount);
+        }
+
+        [Test]
+        public void Resolve_Transient_All_NonGeneric_Implicit()
+        {
+            const int EXPECTED_COUNT = 2;
+
+            var container = Container.Create();
+
+            container.Register<IColor>(typeof(ColorRed), typeof(ColorBlue));
+
+            var colorsObject = container.Resolve(typeof(IColor[]));
+            var colors = (object[])colorsObject;
+            var actualCount = colors.Length;
+
+            Assert.AreEqual(EXPECTED_COUNT, actualCount);
+        }
+
+        [Test]
+        public void Resolve_Singleton_Contract()
+        {
+            var container = Container.Create();
+
+            container.Register<ITimeMachine, TimeMachineDelorean>(Scope.Singleton);
+
+            var machineFirst = container.Resolve<ITimeMachine>();
+            var machineSecond = container.Resolve<ITimeMachine>();
+
+            Assert.AreSame(machineFirst, machineSecond);
+        }
+
+        [Test]
+        public void Resolve_Singleton_Result()
+        {
+            var container = Container.Create();
+
+            container.Register<TimeMachineDelorean>(Scope.Singleton);
+
+            var machineFirst = container.Resolve<TimeMachineDelorean>();
+            var machineSecond = container.Resolve<TimeMachineDelorean>();
+
+            Assert.AreSame(machineFirst, machineSecond);
+        }
+
+        [Test]
+        public void Resolve_Singleton_Result_Throws_TypeNotRegisteredException()
+        {
+            var container = Container.Create();
+
+            container.Register<ITimeMachine, TimeMachineDelorean>(Scope.Singleton);
+
+            Assert.Throws(typeof(TypeNotRegisteredException), () =>
+            {
+                container.Resolve<TimeMachineDelorean>();
+            });
+        }
+
+        [Test]
+        public void Resolve_Factory()
+        {
+            var container = Container.Create();
+
+            container.RegisterFactory<CarFactory>();
+
+            var factory = container.Resolve<CarFactory>();
+            var result = factory.Create();
+
+            Assert.IsInstanceOf<ICar>(result);
+        }
+    }
+}
