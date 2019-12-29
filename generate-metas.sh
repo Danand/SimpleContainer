31f241d6ea1d6b1568621930c31df689d801816b
Andrey Danilov <danand@inbox.ru>
2019-12-29 17:09:07 +0300

    Add `.meta` generation for folders

--- generate-metas.sh
+++ generate-metas.sh
@@ -7,7 +7,11 @@ generate_guid () {
 }
 
 find_cs_src () {
-    find . -name "*.cs" -not -path "./obj/*" -not -path "./bin/*"
+    find . -name "*.cs" -not -path "./obj*" -not -path "./bin*"
+}
+
+find_dirs () {
+    find . -type d -not -path "./obj*" -not -path "./bin*" -not -path "."
 }
 
 cd $1
@@ -15,19 +19,29 @@ cd $1
 for script in $(find_cs_src)
 do
     meta_path="${script}.meta"
-    guid=$(generate_guid)
-    timestamp=$(date +%s%3N)
-    meta_content="fileFormatVersion: 2\nguid: ${guid}\ntimeCreated: ${timestamp}"
-    echo -e $meta_content > "${meta_path}"
-    echo "Meta file generated: ${meta_path}"
+    
+    if [ -f "${meta_path}" ]; then
+        echo "Meta file is already exists: ${meta_path}"
+    else
+        guid=$(generate_guid)
+        timestamp=$(date +%s%3N)
+        meta_content="fileFormatVersion: 2\nguid: ${guid}\ntimeCreated: ${timestamp}\c"
+        echo -e "${meta_content}" > "${meta_path}"
+        echo "Meta file generated: ${meta_path}"
+    fi
 done
 
-# TODO: generate folders meta, here is template.
-#fileFormatVersion: 2
-#guid: 999f6d192451417409be055c698088a0
-#folderAsset: yes
-#DefaultImporter:
-#  externalObjects: {}
-#  userData: 
-#  assetBundleName: 
-#  assetBundleVariant: 
+for dir in $(find_dirs)
+do
+    meta_path="${dir}.meta"
+    
+    if [ -f "${meta_path}" ]; then
+        echo "Meta file is already exists: ${meta_path}"
+    else
+        guid=$(generate_guid)
+        meta_content="fileFormatVersion: 2\nguid: ${guid}\nfolderAsset: yes\nDefaultImporter:\n  externalObjects: {}\n  userData: \n  assetBundleName: \n  assetBundleVariant: \c"
+        echo -e "${meta_content}" > "${meta_path}"
+        echo "Meta file generated: ${meta_path}"
+    fi
+done
+
