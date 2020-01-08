29e98c7f76af4a4a79bceb567816c7050472aeb6
Andrey Danilov <danand@inbox.ru>
2020-01-08 18:50:08 +0300

    Modify release script with info commit

--- release-package.sh
+++ release-package.sh
@@ -2,11 +2,24 @@
 #
 # Releases package for Unity Package Manager.
 
+# FUNCTIONS:
+mv_to_prefix () {
+  mv README.md "$1" && git add "$1"
+}
+
+# EXECUTION:
 tag="$1"
+prefix="SimpleContainer.Unity/Assets"
 source_branch=$(git rev-parse --abbrev-ref HEAD)
 release_branch="release-package-unity/${tag}"
 
-git subtree split --prefix=SimpleContainer.Unity/Assets ${tag} --squash -b ${release_branch}
+mv_to_prefix README.md "${prefix}"
+mv_to_prefix LICENSE.md "${prefix}"
+mv_to_prefix CHANGELOG.md "${prefix}"
+
+git commit -m "Add package details for ${tag}"
+
+git subtree split --prefix="${prefix}" ${tag} --squash -b ${release_branch}
 git checkout ${release_branch} --force
 
 root_commit=$(git rev-list --max-parents=0 --abbrev-commit HEAD)
