7a48eecad7f1eaf97eb3e213d6e6ffb4b090caf7
Andrey Danilov <danand@inbox.ru>
2020-02-25 09:45:56 +0300

    Add draft dependency graph classes

---
+++ SimpleContainer.Unity/Assets/SimpleContainer/DependencyGraph.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleContainer
+{
+    public sealed class DependencyGraph
+    {
+        public IList<DependencyNode> RootNodes { get; set; }
+
+        public void Register<TContract, TResult>(Scope scope, TResult instance)
+        {
+            RootNodes.Add(new DependencyNode
+            {
+                ContractType = typeof(TContract),
+                ResultType = typeof(TResult),
+                Scope = scope,
+                Instance = instance
+            });
+        }
+
+        public void Link()
+        {
+            foreach (var rootNode in RootNodes)
+            {
+                PopulateChildNodes(rootNode);
+            }
+
+            foreach (var rootNode in RootNodes)
+            {
+                LinkDependenciesRecursive(rootNode, RootNodes);
+            }
+        }
+
+        public void Resolve()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PopulateChildNodes(DependencyNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LinkDependenciesRecursive(DependencyNode node, IList<DependencyNode> rootNodes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpleContainer.Unity/Assets/SimpleContainer/DependencyNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleContainer
+{
+    public sealed class DependencyNode
+    {
+        public Scope Scope { get; set; }
+
+        public Type ContractType { get; set; }
+
+        public Type ResultType { get; set; }
+
+        public object Instance { get; set; }
+
+        public IList<DependencyNode> ChildNodes { get; } = new List<DependencyNode>();
+    }
+}
