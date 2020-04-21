8918fb98bb71be88162aa8ee9f77e586b8c48fce
Andrey Danilov <danand@inbox.ru>
2020-04-22 01:23:01 +0300

    Add Unity tests placeholder

---
+++ SimpleContainer.Unity/Assets/SimpleContainer.Unity/Tests/RootTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+using SimpleContainer.Unity.Roots;
+
+using UnityEngine;
+
+namespace SimpleContainer.Unity.Tests
+{
+    public class RootTests
+    {
+        [Test]
+        public void OverrideFrom_Pass()
+        {
+            var projectRoot = new GameObject().AddComponent<UnityProjectRootManual>();
+            var sceneRoot = new GameObject().AddComponent<UnitySceneRootManual>();
+
+            // TODO: Add real checks.
+
+            Assert.NotNull(projectRoot);
+            Assert.NotNull(sceneRoot);
+        }
+    }
+}
