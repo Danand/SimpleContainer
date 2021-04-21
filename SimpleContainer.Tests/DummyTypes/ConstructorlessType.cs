df24739714b35733a95bbc1a01640d766c5295df
Andrey Danilov <danand@inbox.ru>
2021-04-21 08:31:10 +0300

    Add more verbose info for `ConstructorNotFoundException`

--- SimpleContainer.Tests/DummyTypes/ConstructorlessType.cs
+++ SimpleContainer.Tests/DummyTypes/ConstructorlessType.cs
@@ -3,5 +3,9 @@
     public sealed class ConstructorlessType
     {
         private ConstructorlessType() { }
+
+        internal ConstructorlessType(int x) { }
+
+        protected ConstructorlessType(float y) { }
     }
 }
--- SimpleContainer.Unity/Assets/SimpleContainer/Exceptions/ConstructorNotFoundException.cs
+++ SimpleContainer.Unity/Assets/SimpleContainer/Exceptions/ConstructorNotFoundException.cs
@@ -1,9 +1,52 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace SimpleContainer.Exceptions
 {
     public sealed class ConstructorNotFoundException : Exception
     {
-        public ConstructorNotFoundException(Type type) : base($"Result type '{type.Name}' does not contain any public constructors!") { }
+        private enum AccessModifier
+        {
+            Private,
+            Protected,
+            Internal,
+            Public,
+            ProtectedInternal
+        }
+
+        public ConstructorNotFoundException(Type type) : base($"Result type '{type.Name}' does not contain any public instance constructors!" +
+                                                              $"{Environment.NewLine}" +
+                                                              "Other constructors:" +
+                                                              $"{Environment.NewLine}" +
+                                                              $"{FormatConstructors(type)}") { }
+
+        private static string FormatConstructors(Type type)
+        {
+            return string.Join(
+                Environment.NewLine,
+                type.GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance)
+                    .Select(constructor => $"{GetAccessModifier(constructor)} {constructor}"));
+        }
+
+        private static AccessModifier GetAccessModifier(ConstructorInfo methodInfo)
+        {
+            if (methodInfo.IsPrivate)
+                return AccessModifier.Private;
+
+            if (methodInfo.IsFamily)
+                return AccessModifier.Protected;
+
+            if (methodInfo.IsFamilyOrAssembly)
+                return AccessModifier.ProtectedInternal;
+
+            if (methodInfo.IsAssembly)
+                return AccessModifier.Internal;
+
+            if (methodInfo.IsPublic)
+                return AccessModifier.Public;
+
+            throw new ArgumentException("Did not find access modifier", "methodInfo");
+        }
     }
 }
