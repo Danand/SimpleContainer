8c09e048c49b97c3b5ddd6f76493bf7acffb10eb
Andrey Danilov <danand@inbox.ru>
2021-04-21 08:27:12 +0300

    Add dummy constructor for `ActivatorReflection`

--- SimpleContainer.Unity/Assets/SimpleContainer/Activators/ActivatorReflection.cs
+++ SimpleContainer.Unity/Assets/SimpleContainer/Activators/ActivatorReflection.cs
@@ -6,6 +6,8 @@ namespace SimpleContainer.Activators
 {
     public sealed class ActivatorReflection : IActivator
     {
+        public ActivatorReflection() { }
+
         object IActivator.CreateInstance(ConstructorInfo constructor, object[] args)
         {
             return constructor.Invoke(args);
